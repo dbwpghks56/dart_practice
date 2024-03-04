ce4fa7d6f45ac336ef57effa556aac7227041c77
wpghks <wpghks56@naver.com>
2024-03-04 15:31:00 +0900

    [Feature] Study Project Init

---
+++ apple_store/android/app/src/main/kotlin/com/example/apple_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apple_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ apple_store/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Hello Apple Store",
+            style: TextStyle(fontSize: 26),
+          ),
+        ),
+      ),
+    );
+  }
+}
