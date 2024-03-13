85ff50bf46990b601098815fbd991c3bcceeb7bc
wpghks <wpghks56@naver.com>
2024-03-13 09:47:26 +0900

    [ Feature ] AssetIcon

---
+++ house_of_tomorrow/lib/theme/component/asset_icon.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
+
+class AssetIcon extends ConsumerWidget {
+  const AssetIcon(
+    this.icon, {
+    super.key,
+    this.color,
+    this.size,
+  });
+
+  final String icon;
+  final Color? color;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SvgPicture.asset(
+      'assets/icons/$icon.svg',
+      width: size,
+      height: size,
+      colorFilter: ColorFilter.mode(
+        color ?? ref.watch(themeProvider).color.text,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
