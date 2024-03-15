db32b520943f2a6c6857839366381ff23e3be2da
wpghks <wpghks56@naver.com>
2024-03-15 15:01:34 +0900

    [ Feature ] product bottom sheet

--- house_of_tomorrow/lib/src/view/product/product_view.dart
+++ house_of_tomorrow/lib/src/view/product/product_view.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/view/product/widget/product_bottomsheet.dart';
 import 'package:house_of_tomorrow/src/view/product/widget/product_color_preview.dart';
 import 'package:house_of_tomorrow/src/view/product/widget/product_desc.dart';
 import 'package:house_of_tomorrow/theme/component/color_picker.dart';
-import 'package:house_of_tomorrow/theme/component/counter_button.dart';
 import 'package:house_of_tomorrow/theme/component/pop_button.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 
@@ -37,34 +37,42 @@ class ProductView extends HookConsumerWidget {
         leading: const PopButton(),
         titleSpacing: 0,
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(
-          vertical: 32,
-        ),
-        child: Wrap(
-          runSpacing: 32,
-          alignment: WrapAlignment.center,
-          children: [
-            ProductColorPreview(
-              colorIndex: colorIndex.value,
-              product: product,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(
+                vertical: 32,
+              ),
+              child: Wrap(
+                runSpacing: 32,
+                alignment: WrapAlignment.center,
+                children: [
+                  ProductColorPreview(
+                    colorIndex: colorIndex.value,
+                    product: product,
+                  ),
+                  ColorPicker(
+                    colorIndex: colorIndex.value,
+                    colorList: product.productColorList.map((e) {
+                      return e.color;
+                    }).toList(),
+                    onColorSelected: onColorIndexChange,
+                  ),
+                  ProductDesc(
+                    product: product,
+                  ),
+                ],
+              ),
             ),
-            ColorPicker(
-              colorIndex: colorIndex.value,
-              colorList: product.productColorList.map((e) {
-                return e.color;
-              }).toList(),
-              onColorSelected: onColorIndexChange,
-            ),
-            ProductDesc(
-              product: product,
-            ),
-            CounterButton(
-              count: count.value,
-              onChanged: onCountChange,
-            ),
-          ],
-        ),
+          ),
+          ProductBottonSheet(
+            count: count.value,
+            product: product,
+            onCountChange: onCountChange,
+            onAddtToCartPressed: () {},
+          ),
+        ],
       ),
     );
   }
---
+++ house_of_tomorrow/lib/src/view/product/widget/product_bottomsheet.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:house_of_tomorrow/src/model/product.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
+import 'package:house_of_tomorrow/theme/component/bottom_sheet/base_bottom_sheet.dart';
+import 'package:house_of_tomorrow/theme/component/button/button.dart';
+import 'package:house_of_tomorrow/theme/component/counter_button.dart';
+import 'package:house_of_tomorrow/util/helper/intl_helper.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
+
+class ProductBottonSheet extends ConsumerWidget {
+  const ProductBottonSheet({
+    super.key,
+    required this.count,
+    required this.product,
+    required this.onCountChange,
+    required this.onAddtToCartPressed,
+  });
+
+  final int count;
+  final Product product;
+  final void Function(int count) onCountChange;
+  final void Function() onAddtToCartPressed;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeData = ref.watch(themeProvider);
+
+    return BaseBottomSheet(
+      padding: const EdgeInsets.only(
+        top: 32,
+        bottom: 16,
+        left: 16,
+        right: 16,
+      ),
+      child: Wrap(
+        runSpacing: 16,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  S.current.quantity,
+                  style: themeData.typo.headline3,
+                ),
+              ),
+              CounterButton(
+                count: count,
+                onChanged: onCountChange,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  S.current.totalPrice,
+                  style: themeData.typo.headline3,
+                ),
+              ),
+              Text(
+                IntlHelper.currency(
+                  symbol: product.priceUnit,
+                  number: product.price * count,
+                ),
+                style: themeData.typo.headline3.copyWith(
+                  color: themeData.color.primary,
+                ),
+              ),
+            ],
+          ),
+          Button(
+            onPressed: onAddtToCartPressed,
+            width: double.infinity,
+            size: ButtonSize.large,
+            text: S.current.addToCart,
+          ),
+        ],
+      ),
+    );
+  }
+}
