aa4e590c2dda7c0a6b0e79f1920e199e70a95d8f
wpghks <wpghks56@naver.com>
2024-03-15 14:14:28 +0900

    [ Feature ] Product Description

--- house_of_tomorrow/lib/src/view/product/product_view.dart
+++ house_of_tomorrow/lib/src/view/product/product_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:house_of_tomorrow/src/model/product.dart';
 import 'package:house_of_tomorrow/src/view/product/widget/product_color_preview.dart';
+import 'package:house_of_tomorrow/src/view/product/widget/product_desc.dart';
 import 'package:house_of_tomorrow/theme/component/color_picker.dart';
 import 'package:house_of_tomorrow/theme/component/pop_button.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
@@ -35,22 +36,30 @@ class ProductView extends HookConsumerWidget {
         leading: const PopButton(),
         titleSpacing: 0,
       ),
-      body: Wrap(
-        runSpacing: 32,
-        alignment: WrapAlignment.center,
-        children: [
-          ProductColorPreview(
-            colorIndex: colorIndex.value,
-            product: product,
-          ),
-          ColorPicker(
-            colorIndex: colorIndex.value,
-            colorList: product.productColorList.map((e) {
-              return e.color;
-            }).toList(),
-            onColorSelected: onColorIndexChange,
-          ),
-        ],
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(
+          vertical: 32,
+        ),
+        child: Wrap(
+          runSpacing: 32,
+          alignment: WrapAlignment.center,
+          children: [
+            ProductColorPreview(
+              colorIndex: colorIndex.value,
+              product: product,
+            ),
+            ColorPicker(
+              colorIndex: colorIndex.value,
+              colorList: product.productColorList.map((e) {
+                return e.color;
+              }).toList(),
+              onColorSelected: onColorIndexChange,
+            ),
+            ProductDesc(
+              product: product,
+            ),
+          ],
+        ),
       ),
     );
   }
