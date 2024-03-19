fdd8669a803308881fa5c872e69727d7218ea65a
wpghks <wpghks56@naver.com>
2024-03-19 11:41:51 +0900

    [ Feature ] CartBottomSheet

--- house_of_tomorrow/lib/src/view/cart/cart_view.dart
+++ house_of_tomorrow/lib/src/view/cart/cart_view.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:house_of_tomorrow/src/model/cart_item.dart';
 import 'package:house_of_tomorrow/src/service/cart_service.dart';
+import 'package:house_of_tomorrow/src/view/cart/widget/cart_bottom_sheet.dart';
 import 'package:house_of_tomorrow/src/view/cart/widget/cart_empty.dart';
 import 'package:house_of_tomorrow/src/view/cart/widget/cart_item_tile.dart';
 import 'package:house_of_tomorrow/theme/component/pop_button.dart';
+import 'package:house_of_tomorrow/util/helper/intl_helper.dart';
 import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
 
 class CartView extends ConsumerWidget {
@@ -13,6 +15,8 @@ class CartView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final cartData = ref.watch(cartProvider);
+    final selectedCartData =
+        ref.watch(cartProvider.notifier).getSelectedCartItemList();
 
     return Scaffold(
       appBar: AppBar(
@@ -20,27 +24,51 @@ class CartView extends ConsumerWidget {
         leading: const PopButton(),
         titleSpacing: 0,
       ),
-      body: cartData.isEmpty
-          ? const CartEmpty()
-          : ListView.builder(
-              itemCount: cartData.length,
-              itemBuilder: (context, index) {
-                final CartItem cartItem = cartData[index];
+      body: Column(
+        children: [
+          Expanded(
+            child: cartData.isEmpty
+                ? const CartEmpty()
+                : ListView.builder(
+                    itemCount: cartData.length,
+                    itemBuilder: (context, index) {
+                      final CartItem cartItem = cartData[index];
 
-                return CartItemTile(
-                  cartItem: cartItem,
-                  onPressed: () {
-                    ref.read(cartProvider.notifier).update(index,
-                        cartItem.copyWith(isSelected: !cartItem.isSelected));
-                  },
-                  onCountChanged: (count) {
-                    ref
-                        .read(cartProvider.notifier)
-                        .update(index, cartItem.copyWith(count: count));
-                  },
-                );
-              },
-            ),
+                      return CartItemTile(
+                        cartItem: cartItem,
+                        onPressed: () {
+                          ref.read(cartProvider.notifier).update(
+                                index,
+                                cartItem.copyWith(
+                                  isSelected: !cartItem.isSelected,
+                                ),
+                              );
+                        },
+                        onCountChanged: (count) {
+                          ref.read(cartProvider.notifier).update(
+                                index,
+                                cartItem.copyWith(
+                                  count: count,
+                                ),
+                              );
+                        },
+                      );
+                    },
+                  ),
+          ),
+          CartBottomSheet(
+            totalPrice: selectedCartData.isEmpty
+                ? '0'
+                : IntlHelper.currency(
+                    symbol: selectedCartData.first.product.priceUnit,
+                    number: selectedCartData.fold(0, (prev, curr) {
+                      return prev + (curr.product.price * curr.count);
+                    })),
+            selectedCartItemList: cartData,
+            onCheckOutPressed: () {},
+          )
+        ],
+      ),
     );
   }
 }
